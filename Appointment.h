c47acc5e17d3ce0f19cda8f32170ed2a436fd6a6
tasneem821 <11410120220089@stud.cu.edu.eg>
2024-11-24 23:17:21 +0200

    Add files via upload

---
+++ Appointment.h
@@ -0,0 +1,12 @@
+//
+// Created by tasnim on 11/24/2024.
+//
+
+#ifndef ASSIGNMENT_APPOINTMENT_H
+#define ASSIGNMENT_APPOINTMENT_H
+class Appointment{
+    char AppointmentID[15];
+    char AppointmentDate[30];
+    char DoctorID[15];
+};
+#endif //ASSIGNMENT_APPOINTMENT_H
---
+++ Doctor.h
@@ -0,0 +1,14 @@
+//
+// Created by tasnim on 11/24/2024.
+//
+
+#ifndef ASSIGNMENT_DOCTOR_H
+#define ASSIGNMENT_DOCTOR_H
+class Doctor{
+char DoctorID[15];
+char DoctorName[30];
+char Address[30];
+
+
+};
+#endif //ASSIGNMENT_DOCTOR_H
